b2f09920c7329d4fc27278dd5c918ad9525a0e96
zhangzhangc <2667520710@qq.com>
2022-05-23 16:21:47 +0800

    lab2

---
+++ src/main/java/com/lab2/ZhangchunFilter.java
@@ -0,0 +1,18 @@
+package com.lab2;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+@WebFilter(filterName = "YourNameFilter")
+public class ZhangchunFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+    public void destroy() {
+    }
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        System.out.println("YourNameFilter-->before chain");
+        chain.doFilter(request, response);
+        System.out.println("YourNameFilter-->after chain");
+    }
+}
